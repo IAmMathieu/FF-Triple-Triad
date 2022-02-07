1298659fd956854279fc4ebf515dc2abde841e83
AtomicMathO <Math.Oclock-Atome@protonmail.com>
2022-02-07 09:35:08 +0100

    Etape 3.1:
    - Installation du module express-session
    - Import du module express-session dans index.js
    - Initialisation de la session dans index.js par app.use(session({...})

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const session = require('express-session')
+
 dotenv.config();
 
 const PORT = process.env.PORT || 1234;
 const router = require('./app/router');
 
 const app = express();
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, maxAge: 3600000 } // Session d'une heure
+}))
 
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
