044a46c64ee58afe839835439c9b7fe8e00cc544
AtomicMathO <Math.Oclock-Atome@protonmail.com>
2022-02-07 09:56:31 +0100

    Etape 3.2 et 3.3:
    - Creation de deckBuilderController qui regroupera toutes les logiques de creations et modifications du deck
    - Ajout de la function deckCheck a deckBuilderController qui vérifie si un cookie de session 'deck' existe ou non et le crée au besoin
    - Ajout de la méthode addToDeck a deckBuilderController qui ajout une carte au deck, si le deck ne contient pas deja la carte et si le deck contient moins de 5 cartes
    - Ajout de la méthode deckPage qui se charge du rendu de la view deck en renvoyant les informations du cookie deck charger de contenir les cartes selectionnées

---
+++ app/controllers/deckBuilderController.js
@@ -0,0 +1,37 @@
+const dataMapper = require('../dataMapper.js');
+
+function deckCheck(req) {
+    if(!req.session.deck) {
+        req.session.deck = []
+    }
+}
+
+const deckBuilderController = {
+    deckPage: async (req, res) => {
+        deckCheck(req);
+
+        try {
+            let deck = []
+
+            for (let id of req.session.deck) {
+                const card = await dataMapper.getCard(id);
+                deck.push(card)
+            }
+            res.render('deck', { deck, title: 'Votre deck:' });
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    addToDeck: (req, res) => {
+        deckCheck(req);
+        const cardId = Number(req.params.id);
+        if(req.session.deck.length < 5) {
+            if(!req.session.deck.find( (id) => id === cardId)) {
+                req.session.deck.push(cardId);
+            }
+            res.redirect('/deck');
+        }
+    },
+};
+
+module.exports = deckBuilderController;
--- app/router.js
+++ app/router.js
@@ -3,12 +3,18 @@ const router = express.Router();
 
 const mainController = require('./controllers/mainController');
 const searchController = require('./controllers/searchController');
+const deckBuilderController = require('./controllers/deckBuilderController');
+
 
 
 router.get('/', mainController.homePage);
 router.get('/card/:id', mainController.cardPage);
+
 router.get('/search', searchController.searchPage);
 router.get('/search/element', searchController.searchByElement);
+
+router.get('/deck', deckBuilderController.deckPage);
+router.get('/deck/add/:id', deckBuilderController.addToDeck);
 
 
 module.exports = router;
