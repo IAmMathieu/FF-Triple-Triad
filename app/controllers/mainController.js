1f2b70f5df4540901d14c41aa078e1fdf68ec6f8
AtomicMathO <Math.Oclock-Atome@protonmail.com>
2022-02-07 09:16:47 +0100

    Etape 1:
    - Ajout de la route vers la page détaillée d'une carte
    - Creation de la view cardDetails.ejs (avec un peu de css mais pas trop)
    - Ajout dans le dataMapper de la fonction getCard pour récupérer les informations d'une carte par son Id
    - Ajout dans mainController de l'appel a getCard et rendre le résultat dans la view

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,17 +1,30 @@
 const dataMapper = require('../dataMapper.js');
 
 const mainController = {
-  homePage: (req, res) => {
-    dataMapper.getAllCards( (err, results) => {
-      if(err) {
-        console.error(err);
-        return;
-      } 
+  homePage: async (req, res) => {
+    try {
+      const cards = await dataMapper.getAllCards(); 
       res.render('cardList', {
-        cards: results.rows,
+        cards: cards,
         title: 'Liste des cartes'
       })
-    });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send('Internal Server Error')
+    }
+  },
+  cardPage: async (req, res) => {
+    try {
+      const cardId = req.params.id;
+      const cards = await dataMapper.getCard(cardId);
+      res.render('cardDetails', {
+        card: cards,
+        title: 'Détail de la carte'
+      })
+    } catch (error) {
+      console.log(error);
+      res.status(500).send('Internal Server Error')
+    }
   }
 };
 
--- app/dataMapper.js
+++ app/dataMapper.js
@@ -2,12 +2,20 @@ const database = require('./database');
 
 const dataMapper = {
 
-  getAllCards: function (callback) {
+  getAllCards: async () => {
     const query = {
       text : `SELECT * FROM "card"`
     };
-    database.query(query, callback);
-  }
+    const results = await database.query(query);
+    return results.rows;
+  },
+  getCard: async (cardId) => {
+    const query = {
+      text: `SELECT * FROM card WHERE card.id = ${cardId}`
+    }
+    const result = await database.query(query)
+    return result.rows[0]
+  },
   
 };
 
